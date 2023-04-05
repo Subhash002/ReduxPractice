da7c8b4bc2bdca33bab621241ac6ddd32bf7b070
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-04-05 21:48:32 +0530

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,40 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    }
+  }
+});
+
+const songSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songSlice.reducer,
+    movies: moviesSlice.reducer
+  }
+});
+
+export { store };
+export const { addSong, removeSong } = songSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
